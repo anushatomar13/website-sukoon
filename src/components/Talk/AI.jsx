b25d138f798ee5126f7ac17ea81e5c53d663fd93
Anusha Tomar <anushatoma913@gmail.com>
2024-08-20 02:56:47 +0530

    linked

--- src/components/Talk/AI.jsx
+++ src/components/Talk/AI.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';  
 import Blob from "./Blob";
 
 function AI() {
   return (
     <div className='bg-[#A79BD2]'>
       <div className='text-white font-Roca text-3xl sm:text-3xl md:text-4xl text-center px-4 py-10 sm:p-20'>
-  Meet Sky!<br /><br />Sukoon's empathetic AI voice, blending digital intelligence with a caring tone for 24/7 support 💚
-    
+        Meet Sky!<br /><br />Sukoon's empathetic AI voice, blending digital intelligence with a caring tone for 24/7 support 💚
       </div>
       <Link to='https://sky-by-sukoon.vercel.app/'>
-      <div className='flex justify-center items-center pb-10 sm:pb-20 overflow-hidden'>
-        <Blob />
-      </div>
-        </Link>
+        <div className='flex justify-center items-center pb-10 sm:pb-20 overflow-hidden'>
+          <Blob />
+        </div>
+      </Link>
     </div>
   );
 }
